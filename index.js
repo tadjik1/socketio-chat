6b1065620ea8a0615aa0ebef3cfe23a93b6389e3
Sergey Zelenov <mail@zelenov.su>
2019-06-20 20:52:27 +0200

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require('./socket');
 const config = require('./config');
 
 const server = app.listen(config.port, () => {
-  console.log('App is running on http://localhost:3000 asdasd');
+  console.log('App is running on http://localhost:3000');
 });
 
 socket(server);
